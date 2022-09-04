1c8b25364c3bddf3c74479277e5b7ef1cb819edc
adi1090x <adi1090x@gmail.com>
2022-09-04 13:28:22 +0530

    Fixed network menu password issue and remove few binds causing issues with app shortcuts

--- archcraft-dwm/source/config.def.h
+++ archcraft-dwm/source/config.def.h
@@ -322,8 +322,6 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
     { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    { ClkClientWin,         ControlMask,    Button1,        dragmfact,      {0} },
-    { ClkClientWin,         ControlMask,    Button3,        dragcfact,      {0} },
     { ClkTagBar,            0,              Button1,        view,           {0} },
     { ClkTagBar,            0,              Button3,        toggleview,     {0} },
     { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
